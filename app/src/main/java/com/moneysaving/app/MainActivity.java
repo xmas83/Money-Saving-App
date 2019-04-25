829a69678918904f39f80762c0f0289508156146
ville <villetorstensson@hotmail.com>
2019-04-25 11:05:23 +0200

    LoginPage

--- app/src/main/java/com/moneysaving/app/MainActivity.java
+++ app/src/main/java/com/moneysaving/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.moneysaving.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Test");
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
