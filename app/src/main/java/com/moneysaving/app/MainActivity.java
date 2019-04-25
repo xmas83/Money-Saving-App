0854f30e9281e332eadbde8d6eb5be910ee0b0cf
Hamid Alijani <alijani.hamid@yahoo.com>
2019-04-25 19:00:58 +0200

    SQLiteDatabase class and user class added

--- app/src/main/java/com/moneysaving/app/MainActivity.java
+++ app/src/main/java/com/moneysaving/app/MainActivity.java
@@ -11,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
+
     }
 }
