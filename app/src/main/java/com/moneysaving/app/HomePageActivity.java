4190e453a9435c8d86a263284d0e8ec2a8db654f
martinrydstrom <rydstrom.martin@gmail.com>
2019-05-08 13:18:41 +0200

    Implemented some tests

--- app/src/main/java/com/moneysaving/app/HomePageActivity.java
+++ app/src/main/java/com/moneysaving/app/HomePageActivity.java
@@ -1,7 +1,20 @@
 package com.moneysaving.app;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -10,5 +23,7 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
     }
 }
