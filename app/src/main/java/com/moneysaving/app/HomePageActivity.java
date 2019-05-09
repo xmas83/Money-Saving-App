697a2eaf44f63afc0acaab3d732900b97891a893
Paolo Perez <pao_9517@hotmail.com>
2019-05-09 16:45:31 +0200

    Budget and balance

--- app/src/main/java/com/moneysaving/app/HomePageActivity.java
+++ app/src/main/java/com/moneysaving/app/HomePageActivity.java
@@ -1,9 +1,11 @@
 package com.moneysaving.app;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
+import android.hardware.SensorManager;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,9 +16,11 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 
+import com.squareup.seismic.ShakeDetector;
+
 import java.util.ArrayList;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements ShakeDetector.Listener {
 
 
     @Override
@@ -24,6 +28,21 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        SensorManager sensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        ShakeDetector shakeDetector = new ShakeDetector (this);
+
+        shakeDetector.start(sensorManager);
+
+
+
+    }
+
+    @Override
+    public void hearShake() {
+
+        Intent intent1 = new Intent(HomePageActivity.this, BalanceActivity.class);
+        startActivity(intent1);
 
     }
 }
