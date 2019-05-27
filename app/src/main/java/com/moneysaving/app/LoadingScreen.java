727b03a79cee0ea3fc3d01905f4a21dc0ffd316f
ville <villetorstensson@hotmail.com>
2019-05-27 16:14:45 +0200

    Loading Screen

---
+++ app/src/main/java/com/moneysaving/app/LoadingScreen.java
@@ -0,0 +1,38 @@
+package com.moneysaving.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        progressBar = findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.text_view);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+
+
+        progressAnimation();
+
+    }
+
+    public void progressAnimation() {
+        ProgressBarAnimation anim = new ProgressBarAnimation(this, progressBar,textView, 0, 100);
+        anim.setDuration(8000);
+        progressBar.setAnimation(anim);
+    }
+}
--- app/src/main/java/com/moneysaving/app/MainActivity.java
+++ app/src/main/java/com/moneysaving/app/MainActivity.java
@@ -10,52 +10,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv;
-    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = new Intent(MainActivity.this, LoadingScreen.class);
+        startActivity(intent);
 
-        // Initialize components
-        tv = (TextView) findViewById(R.id.intro_text);
-        iv = (ImageView) findViewById(R.id.intro_image);
-
-        // Initialize the Animation/transition
-        Animation myAnimation = AnimationUtils.loadAnimation(this, R.anim.mytransition);
-
-        tv.startAnimation(myAnimation);
-        iv.startAnimation(myAnimation);
-
-        // Transition to SignUp class
-        final Intent intent = new Intent(MainActivity.this, SignUp.class);
-
-
-        // Create a thread and set how long the intro shall be
-        Thread timer = new Thread() {
-
-
-            // Run the thread
-            @Override
-            public void run() {
-                try {
-
-                    sleep(5000);
-                } catch (Exception e) {
-                    e.printStackTrace();
-
-                } finally {
-                    startActivity(intent);
-                    finish();
-                }
-
-                super.run();
-
-            }
-        };
-        timer.start();
     }
 }
 
